2c677f0d0f229587c44ac471ae8bc77258d910d6
Jeff Modell <jmodell@local.busseinc.com>
2023-05-17 14:58:09 -0400

    added suspense boundaries

--- src/app/tickets/it/page.tsx
+++ src/app/tickets/it/page.tsx
@@ -1,6 +1,7 @@
 import { type Ticket } from "@/types/ticket.type";
 import TableRenderer from "@/components/tickets/hydrated";
 import getBaseUrl from "@/lib/baseURL";
+import { Suspense } from "react";
 
 async function getData() {
   const url = new URL(`${getBaseUrl()}/api/tickets`);
@@ -20,7 +21,9 @@ export default async function TicketsITDashboard() {
 
   return (
     <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-      <TableRenderer data={tickets} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <TableRenderer data={tickets} />
+      </Suspense>
     </div>
   );
 }
